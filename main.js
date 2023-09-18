97554f89c2298fab4974160860efde522e19e91e
Nick V <nickviduya.dev@gmail.com>
2023-09-18 21:56:05 +0800

    updated HTMl, CSS and JS added new complete functionality

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const taskInput = document.getElementById("taskInput");
     const addTaskBtn = document.getElementById("addTaskBtn");
     const taskList = document.getElementById("taskList");
     const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+    const completedTasks = document.getElementById("completedTasks");
 
     addTaskBtn.addEventListener("click", addTask);
-    taskInput.addEventListener("keyup", function(event) {
+    taskInput.addEventListener("keyup", function (event) {
         if (event.key === "Enter") {
             addTask();
         }
@@ -21,10 +22,17 @@ document.addEventListener("DOMContentLoaded", function() {
         li.innerHTML = `
             <span>${taskText}</span>
             <button class="delete-btn">Delete</button>
+            <button class="complete-btn">Complete</button>
         `;
 
-        li.querySelector(".delete-btn").addEventListener("click", function() {
+        li.querySelector(".delete-btn").addEventListener("click", function () {
             li.remove();
+        });
+
+        li.querySelector(".complete-btn").addEventListener("click", function () {
+            taskList.removeChild(li);
+            completedTasks.appendChild(li);
+            li.querySelector(".complete-btn").remove();
         });
 
         taskList.appendChild(li);
@@ -32,11 +40,9 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     function clearCompleted() {
-        const completedTasks = taskList.querySelectorAll("li");
-        completedTasks.forEach(function(task) {
-            if (task.querySelector("span").classList.contains("completed")) {
-                task.remove();
-            }
+        const completedTaskItems = completedTasks.querySelectorAll("li");
+        completedTaskItems.forEach(function (taskItem) {
+            taskItem.remove();
         });
     }
 });
